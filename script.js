af97f10a636714ad90b8e13345fef5c71a17c910
PratikJHA29 <jhapratikkumarjha@gmail.com>
2024-04-11 17:52:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your contact form submission logic here
+    // For example, you can use Fetch API to send form data to a backend server
+});
